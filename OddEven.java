acac3e10e900cc7cbc2f5119c822234fcce1f1fc
Muteeqkhan <muteeqkhan.mk@gmail.com>
2021-10-17 10:58:15 +0530

    "basic pattern and logical program"

---
+++ OddEven.java
@@ -0,0 +1,13 @@
+class OddEven{
+	public static void main(String[] args) {
+		int num=18;
+		int rem;
+		rem=num-(num/2)*2;
+		if(rem==0){
+			System.out.println("given number is even");
+		}
+		else
+			System.out.println("odd number");
+	}
+	
+}
